276f61b0b4ec60b60cb0fe8edd5f2c956716a294
williamwjd <william.jose.wjd@gmail.com>
2020-04-01 14:26:21 -0300

    finalizando crud de categories

--- backend/src/app/controllers/CategorieController.js
+++ backend/src/app/controllers/CategorieController.js
@@ -1,16 +1,53 @@
 const Categorie = require('../models/Categorie')
 
-class CategorieController{
-   async index(req, res){
-       const categories = await Categorie.findAll()
-       return res.json(categories)
-   }
+class CategorieController {
+    async index(req, res) {
+        const categories = await Categorie.findAll()
+        return res.json(categories)
+    }
 
-    async store(req, res){
-        const { name } = req.body
-        const categorie = await Categorie.create({  name })
+    async show(req, res){
+        const { categorie_id } = req.params
+
+        const categorie = await Categorie.findByPk(categorie_id)
+        if(!categorie){
+            return res.status(401).json({error:'Categoria não encontrada'})
+        }
+
         return res.json(categorie)
     }
+
+    async store(req, res) {
+        const { name } = req.body
+        const categorie = await Categorie.create({ name })
+        return res.json(categorie)
+    }
+
+    async update(req, res){
+        const { categorie_id } = req.params
+        const { name } = req.body
+
+        const categorie = await Categorie.findByPk(categorie_id)
+        if(!categorie){
+            return res.status(401).json({error:'Categoria não encontrada'})
+        }
+
+        categorie.update({ name })
+        return res.json(categorie)
+    }
+
+    async delete(req, res){
+        const { categorie_id } = req.params
+
+        const categorie = await Categorie.findByPk(categorie_id)
+        if(!categorie){
+            return res.status(401).json({error:'Categoria não encontrada'})
+        }
+
+        categorie.destroy()
+        return res.json({})
+    }
+
 }
 
 module.exports = new CategorieController()
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,5 +6,8 @@ const routes = express.Router()
 
 routes.post('/categories', CategorieController.store)
 routes.get('/categories', CategorieController.index)
+routes.get('/categories/:categorie_id', CategorieController.show)
+routes.put('/categories/:categorie_id', CategorieController.update)
+routes.delete('/categories/:categorie_id', CategorieController.delete)
 
 module.exports = routes
