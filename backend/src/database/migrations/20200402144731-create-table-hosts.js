345eb6778899a51a9f79e613d9205c6a95b83d38
williamwjd <william.jose.wjd@gmail.com>
2020-04-02 12:14:24 -0300

    criando migrations hosts

---
+++ backend/src/database/migrations/20200402144731-create-table-hosts.js
@@ -0,0 +1,56 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('hosts', { 
+        id:{
+          type:Sequelize.INTEGER,
+          primaryKey:true,
+          autoIncrement:true,
+          allowNull:false
+        },
+        description:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        service_tag:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        ram:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        cpu:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        serial:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        patrimony:{
+          type:Sequelize.STRING,
+          allowNull:false
+        },
+        department_id:{
+          type:Sequelize.INTEGER,
+          references:{
+            model:'departments',
+            key:'id'
+          },
+          allowNull:false
+        },
+        created_at:{
+          type:Sequelize.DATE,
+          allowNull:false
+        },
+        updated_at:{
+          type:Sequelize.DATE,
+          allowNull:false
+        }
+      });
+  },
+
+  down: queryInterface => {
+      return queryInterface.dropTable('hosts');
+  }
+};
